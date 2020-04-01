c05b1f003d498188d7d29fc1dae4a55ea59fa5dc
김민재 <minjea456@naver.com>
2020-04-01 19:51:43 +0900

    디비오류문제 수정

--- controller/restaurant/index.js
+++ controller/restaurant/index.js
@@ -2,9 +2,11 @@ const restdistance = require('./restdistance');
 const fdcategory = require('./fdcategory');
 const distancestend = require('./distancestend');
 const restdetail = require('./restdetail');
+const restlike = require('./restlike');
 module.exports = {
   restdistance,
   fdcategory,
   distancestend,
-  restdetail
+  restdetail,
+  restlike
 };
