358cfc63d0cabfac2c8488c6154e725cf0599329
김민재 <minjea456@naver.com>
2020-04-01 23:22:57 +0900

    1. associate 잘못이해한 부분 알게 되고 associate 설정 잘못된 것 수정 2. 식당의 좋아요 갯수 카운트 api 및 user의 좋아요 클릭시 update 및 insert 기능  api

--- controller/restaurant/restlike.js
+++ controller/restaurant/restlike.js
@@ -2,31 +2,24 @@ const jwt = require('jsonwebtoken');
 const { user_like } = require('../../models');
 require('dotenv').config();
 const { sequelize } = require('../../models/index');
+const { QueryTypes } = require('sequelize');
 module.exports = {
   post: (req, res) => {
     try {
-      const token = req.headers.authorization.split(' ')[1];
-      console.log(token);
-      const userobj = jwt.verify(token, process.env.JWT_KEY).data;
-      console.log(userobj);
       let rest_id = req.body.rest_id;
-      if (userobj.id) {
-        let query =
-          'SELECT count(rest_id) as count FROM user_likes group by rest_id where rest_id = ' +
-          rest_id +
-          ' AND check = 1';
-        sequelize
-          .query(query, {
-            type: QueryTypes.SELECT
-          })
-          .then(result => {
-            console.log(result);
-            res.send(result);
-          });
-      } else {
-        console.log('wrong');
-        res.status(400).send('wrong');
-      }
+      let query =
+        'SELECT rest_id, count(rest_id) as count FROM user_likes where rest_id = ' +
+        rest_id +
+        ' AND likecheck = 1 group by rest_id';
+      rest_id;
+      sequelize
+        .query(query, {
+          type: QueryTypes.SELECT
+        })
+        .then(result => {
+          console.log(result);
+          res.send(result[0]);
+        });
     } catch (err) {
       console.log(err);
       res.send('failed');
